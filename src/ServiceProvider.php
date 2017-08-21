befab85d888d9e053e802e348a3aa2e51eff4261
SeniorDevCode <senior.dev.software@gmail.com>
2017-08-21 19:27:03 +0100

    Translate menus using Filter Class

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Container\Container;
 use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
-use JeroenNoten\LaravelAdminLte\Console\AdminLteMakeCommand;
 use JeroenNoten\LaravelAdminLte\Console\MakeAdminLteCommand;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use JeroenNoten\LaravelAdminLte\Http\ViewComposers\AdminLteComposer;
@@ -95,8 +94,6 @@ class ServiceProvider extends BaseServiceProvider
         // Laravel >=5.2 only
         if (class_exists('Illuminate\\Auth\\Console\\MakeAuthCommand')) {
             $this->commands(MakeAdminLteCommand::class);
-        } elseif (class_exists('Illuminate\\Auth\\Console\\AuthMakeCommand')) {
-            $this->commands(AdminLteMakeCommand::class);
         }
     }
 
