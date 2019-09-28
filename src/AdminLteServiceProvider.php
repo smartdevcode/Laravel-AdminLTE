b72a94ba4bf4cfa3143267fcb95ec06ad13c4413
SeniorDevCode <senior.dev.software@gmail.com>
2019-09-28 14:02:22 -0400

    Add missing header tag (#383)

--- src/AdminLteServiceProvider.php
+++ src/AdminLteServiceProvider.php
@@ -7,12 +7,11 @@ use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Container\Container;
 use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
-use JeroenNoten\LaravelAdminLte\Console\AdminLteMakeCommand;
-use JeroenNoten\LaravelAdminLte\Console\MakeAdminLteCommand;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use JeroenNoten\LaravelAdminLte\Console\AdminLteInstallCommand;
 use JeroenNoten\LaravelAdminLte\Http\ViewComposers\AdminLteComposer;
 
-class ServiceProvider extends BaseServiceProvider
+class AdminLteServiceProvider extends BaseServiceProvider
 {
     public function register()
     {
@@ -81,7 +80,7 @@ class ServiceProvider extends BaseServiceProvider
     private function publishAssets()
     {
         $this->publishes([
-            $this->packagePath('resources/assets') => public_path('vendor/adminlte'),
+            $this->packagePath('resources/assets') => public_path(),
         ], 'assets');
     }
 
@@ -92,12 +91,7 @@ class ServiceProvider extends BaseServiceProvider
 
     private function registerCommands()
     {
-        // Laravel >=5.2 only
-        if (class_exists('Illuminate\\Auth\\Console\\MakeAuthCommand')) {
-            $this->commands(MakeAdminLteCommand::class);
-        } elseif (class_exists('Illuminate\\Auth\\Console\\AuthMakeCommand')) {
-            $this->commands(AdminLteMakeCommand::class);
-        }
+        $this->commands(AdminLteInstallCommand::class);
     }
 
     private function registerViewComposers(Factory $view)
